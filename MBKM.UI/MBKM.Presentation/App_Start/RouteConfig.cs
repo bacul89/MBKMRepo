377989984f37c0069433eaf4ad07e83ffd34be72
bacul <baculsanjose@gmail.com>
2021-09-02 09:30:06 +0700

    tambah area portal dan admin

--- MBKM.UI/MBKM.Presentation/App_Start/RouteConfig.cs
+++ MBKM.UI/MBKM.Presentation/App_Start/RouteConfig.cs
@@ -14,10 +14,17 @@ namespace MBKM.Presentation
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { area = "Portal", controller = "Home", action = "Index", id = UrlParameter.Optional }, // Parameter defaults
+                null,
+                new[] { "MBKM.Presentation.Areas.Portal.Controllers" }
+            ).DataTokens.Add("area", "Portal");
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
---
+++ MBKM.UI/MBKM.Presentation/Areas/Portal/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBKM.Presentation.Areas.Portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Portal/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
