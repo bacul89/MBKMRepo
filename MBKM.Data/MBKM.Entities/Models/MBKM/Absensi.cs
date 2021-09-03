63bb8f2f18b2045431dc4c797f13089e680f3936
bacul <baculsanjose@gmail.com>
2021-09-03 09:48:39 +0700

    add projek baru entities

---
+++ MBKM.Data/MBKM.Entities/Models/MBKM/Absensi.cs
@@ -0,0 +1,12 @@
+﻿using MBKM.Entities.Basentities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MBKM.Entities.Models.MBKM
+{
+    public class Absensi : BaseEntity
+    {
+
+    }
+}
---
+++ MBKM.Data/MBKM.Entities/Models/MBKM/JadwalKuliah.cs
@@ -0,0 +1,20 @@
+﻿using MBKM.Entities.Basentities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MBKM.Entities.Models.MBKM
+{
+    public class JadwalKuliah : BaseEntity
+    {
+        public Int64 DosenID { get; set; }
+        public string NamaDosen { get; set; }
+        public Int64 MataKuliah { get; set; }
+        public string KodeMataKuliah { get; set; }
+        public string Hari { get; set; }
+        public DateTime Waktu { get; set; }
+        public int Kuota { get; set; }
+        public string LinkMateri { get; set; }
+
+    }
+}
