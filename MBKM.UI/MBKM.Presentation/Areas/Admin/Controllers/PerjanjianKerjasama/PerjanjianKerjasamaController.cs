8672e148b082026ce91317bedd2c8d82e1a1166d
rifkyar <rifkyrachman21@gmail.com>
2021-09-10 11:02:24 +0700

    Penambahan Halaman Perjanjian Kerjasama [10/9]

---
+++ MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/PerjanjianKerjasama/PerjanjianKerjasamaController.cs
@@ -0,0 +1,23 @@
+﻿using MBKM.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBKM.Presentation.Areas.Admin.Controllers.PerjanjianKerjasama
+{
+    public class PerjanjianKerjasamaController : Controller
+    {
+        private IMenuService _menuService;
+        public PerjanjianKerjasamaController(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+        // GET: Admin/PerjanjianKerjasama
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
