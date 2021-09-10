1c746cc3635ffce6adae8cb1311382a91767635f
abiyogahendra <abiyogakhusus@gmail.com>
2021-09-10 20:12:02 +0700

    Template Email #FE

---
+++ MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/TemplateEmail/TemplateEmailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBKM.Presentation.Areas.Admin.Controllers.TemplateEmail
+{
+    public class TemplateEmailController : Controller
+    {
+        // GET: Admin/EmailTemplate
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
