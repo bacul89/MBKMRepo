c51427d1a6df95d173161ed30b741a88d8864105
abiyogahendra <abiyogakhusus@gmail.com>
2021-09-14 08:38:46 +0700

    CRUD Template Email

---
+++ MBKM.UI/MBKM.Presentation/Scripts/Admin/TemplateEmail/crud.js
@@ -0,0 +1,152 @@
+﻿
+var dTemplateEmail = {}
+
+function getValueOnForm() {
+    dTemplateEmail.TipeMail = $('input[name=inp_tipeEmail]').val();
+    dTemplateEmail.SubjectMail = $('input[name=inp_email]').val();
+    dTemplateEmail.BodyMail = $('textarea[name=inp_bodyEmail]').val();
+    dTemplateEmail.IsActive = $('input[name=inp_status]:checked').val();
+}
+
+function IndexCreateEmailTemplate() {
+    if ($('#created-email-template').length) {
+        $('#TambahMasterLookUp').modal('show');
+    }else{
+        $.ajax({
+            url: '/Admin/TemplateEmail/ModalCreateEmailTemplate',
+            type: 'get',
+            datatype: 'html',
+            success: function (e) {
+                console.log(e);
+                if ($('.data-content-modal').length) {
+                    $('.data-content-modal').remove();
+                }
+                $('#modal-inner').append(e);
+                $('.modal').modal('show');
+                $('.summernote').summernote({
+                    placeholder: 'Input Body Email',
+                    height: 300, // set editor height  
+                    minHeight: null, // set minimum height of editor  
+                });
+            }
+        })
+    } 
+}
+
+function IndexUpdateEmailTemplate(id) {
+    $.ajax({
+        url: '/Admin/TemplateEmail/ModalUpdateEmailTemplate/' + id,
+        type: 'get',
+        datatype: 'html',
+        success: function (e) {
+            console.log(e);
+            if ($('.data-content-modal').length) {
+                $('.data-content-modal').remove();
+            }
+            $('#modal-inner').append(e);
+            $('.modal').modal('show');
+            $('.summernote').summernote({
+                placeholder: 'Input Body Email',
+                height: 300, // set editor height  
+                minHeight: null, // set minimum height of editor  
+            });
+        }
+    })
+}
+
+function IndexViewEmailTemplate(id) {
+    $.ajax({
+        url: '/Admin/TemplateEmail/ModalDetailEmailTemplate/' + id,
+        type: 'get',
+        datatype: 'html',
+        success: function (e) {
+            console.log(e);
+            if ($('.data-content-modal').length) {
+                $('.data-content-modal').remove();
+            }
+            $('#modal-inner').append(e);
+            $('.modal').modal('show');
+        }
+    })
+    
+}
+
+function PostCreate() {
+    getValueOnForm();
+    if (validationCustom()) {
+        var base_url = window.location.origin;
+        $.ajax({
+            url: base_url + '/Admin/TemplateEmail/PostDataEmailTemplate',
+            type: 'post',
+            datatype: 'json',
+            data: JSON.stringify(dTemplateEmail),
+            contentType: 'application/json',
+            success: function (e) {
+                Swal.fire({
+                    title: 'Berhasil',
+                    icon: 'success',
+                    html: 'Template Email Berhasil Ditambahkan',
+                    showCloseButton: true,
+                    showCancelButton: false,
+                    focusConfirm: false,
+                    confirmButtonText: 'OK'
+                })
+                window.location.href = "/Admin/TemplateEmail/index"
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        })
+    } else {
+        Swal.fire({
+            title: 'Oppss',
+            icon: 'warning',
+            html: 'Ada beberapa field yang belum kamu isikan',
+            showCloseButton: true,
+            showCancelButton: false,
+            focusConfirm: false,
+            confirmButtonText: 'OK'
+        })
+    }
+}
+
+function PostUpdate() {
+    getValueOnForm();
+    dTemplateEmail.ID = $('#id_emailTemplate').val();
+    console.log(dTemplateEmail);
+    var base_url = window.location.origin;
+    $.ajax({
+        url: base_url + '/Admin/TemplateEmail/PostUpdateEmailTemplate',
+        type: 'post',
+        datatype: 'json',
+        data: JSON.stringify(dTemplateEmail),
+        contentType: 'application/json',
+        success: function (e) {
+            Swal.fire({
+                title: 'Berhasil',
+                icon: 'success',
+                html: 'Template Email Berhasil Ditambahkan',
+                showCloseButton: true,
+                showCancelButton: false,
+                focusConfirm: false,
+                confirmButtonText: 'OK'
+            })
+/*            window.location.href = "/Admin/TemplateEmail/index"
+*/        },
+        error: function (e) {
+            console.log(e);
+        }
+    })
+    
+    /*Swal.fire({
+        title: 'Oppss',
+        icon: 'warning',
+        html: 'Ada beberapa field yang belum kamu isikan',
+        showCloseButton: true,
+        showCancelButton: false,
+        focusConfirm: false,
+        confirmButtonText: 'OK'
+    })*/
+    
+}
+
---
+++ MBKM.UI/MBKM.Presentation/Scripts/Admin/TemplateEmail/main.js
@@ -0,0 +1,99 @@
+﻿$(document).ready(function () {
+    var t = $('#TableList').DataTable({
+        "columnDefs": [{
+            "searchable": false,
+            "orderable": false,
+            "paging": false,
+            "targets": 0
+        }],
+        "order": [[1, 'asc']],
+        "createdRow": function (row, data, index) {
+            $('td', row).css({
+                'border': '1px solid coral',
+            });
+        }
+    });
+
+    t.on('order.dt search.dt', function () {
+        t.column(0, { search: 'applied', order: 'applied' }).nodes().each(function (cell, i) {
+            cell.innerHTML = i + 1;
+        });
+    }).draw();
+});
+
+function validationCustom() {
+    var isValid;
+    $(".input-data").each(function () {
+        var element = $(this);
+        if (element.val() == "") {
+            return isValid = false;
+        } else {
+            return isValid = true;
+        }
+    });
+    return isValid;
+}
+
+$('#table-data-email-template').DataTable({
+    ajax: {
+        url: '/Admin/TemplateEmail/GetDataEmailTemplate',
+        dataSrc: ''
+    },
+    "columns": [
+        {
+            "data": null,
+            "render": function (data, type, full, meta) {
+                return meta.row + 1;
+            }
+        },
+        {
+            "data": "TipeMail",
+            "render": function (data, type, row, meta) {
+                return '<div class="center">' + data + '</div>';
+            }
+        },
+        {
+            "data": "SubjectMail",
+            "render": function (data, type, row, meta) {
+                return '<div class="center">' + data + '</div>';
+            }
+        },
+        {
+            "data": "BodyMail",
+            "render": function (data, type, row, meta) {
+                return '<div class="center">' + data + '</div>';
+            }
+        },
+        {
+            "data": "IsActive",
+            "render": function (data, type, row, meta) {
+                if (data) {
+                    return '<div class="center">Aktif</div>';
+                } else {
+                    return '<div class="center">Tidak Aktif</div>';
+                }
+
+            }
+        },
+        {
+            "data": "ID",
+            "render": function (data, type, row, meta) {
+                return `<div class="row justify-content-center">
+                            <div class="col" style="text-align:center">
+                                <a href="javascript:void(0)" style="color:black" onclick="IndexUpdateEmailTemplate('${data}')"> <i class="fas fa-edit coral" ></i></a>
+                                <a href="javascript:void(0)" style="color:black" onclick="IndexViewEmailTemplate('${data}')"> <i class="fas fa-file-search coral"></i></a>
+                                <a href="javascript:void(0)" style="color:black" onclick="DeletedTemplateEmail('${data}')">  <i class="fas fa-trash-alt coral"></i></a>
+                            </div>
+                        </div>`;
+            }
+        },
+
+    ],
+    "createdRow": function (row, data, index) {
+        $('td', row).css({
+            'border': '1px solid coral',
+            'border-collapse': 'collapse',
+            'vertical-align' : 'center',
+        });
+    }
+});
