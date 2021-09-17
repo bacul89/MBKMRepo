750b21332431bd8e5d487b67f8de18db29d2eb92
dkusumandaru <darmakusumandaru@gmail.com>
2021-09-17 15:58:08 +0700

    master lookup

---
+++ MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/MasterLookupController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MBKM.Entities.Models;
+using MBKM.Repository.Repositories;
+using MBKM.Services;
+using MBKM.Services.MBKMServices;
+
+
+namespace MBKM.Presentation.Areas.Admin.Controllers
+{
+    public class MasterLookupController : Controller
+    {
+        
+        private Services.ILookupService _lookupService;
+
+        public MasterLookupController(ILookupService lookupService)
+        {
+            _lookupService = lookupService;
+        }
+
+
+
+        // GET: Admin/MasterLookup
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetDataMasterLookup()
+        {
+
+            var data = _lookupService.GetAll();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        /*Modal Created*/
+        public ActionResult ModalCreateMasterLookup()
+        {
+            return View("ModalCreateMasterLookup");
+        }
+
+        [HttpPost]
+        public ActionResult PostDataMasterLookup(Lookup lookup)
+        {
+            Console.WriteLine("Test");
+            Console.WriteLine(lookup);
+            _lookupService.Save(lookup);
+            return Json(lookup);
+        }
+
+        /*Modal Update*/
+        public ActionResult ModalUpdateMasterLookup(int id)
+        {
+            var data = _lookupService.Get(id);
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult PostUpdateMasterLookup(Lookup lookup)
+        {
+
+            Lookup data = _lookupService.Get(lookup.ID);
+            data.Tipe = lookup.Tipe;
+            data.Nama = lookup.Nama;
+            data.Nilai = lookup.Nilai;
+            data.IsActive = lookup.IsActive;
+
+            _lookupService.Save(data);
+
+            return Json(data);
+        }
+
+        [HttpPost]
+        public ActionResult PostDeleteMasterLookup(Lookup lookup)
+        {
+            Lookup data = _lookupService.Get(lookup.ID);
+            data.IsDeleted = lookup.IsDeleted;
+
+            _lookupService.Save(data);
+            return Json(data);
+        }
+
+
+
+        /*Modal Detail*/
+        public ActionResult ModalDetailMasterLookup(int id)
+        {
+            var data = _lookupService.Get(id);
+            return View(data);
+        }
+
+    }
+}
---
+++ MBKM.UI/MBKM.Presentation/Scripts/Admin/MasterLookup/crud.js
@@ -0,0 +1,192 @@
+﻿
+var dMasterLookup = {}
+
+function getValueOnForm() {
+    dMasterLookup.Tipe = $('input[name=inp_tipe]').val();
+    dMasterLookup.Nama = $('input[name=inp_nama]').val();
+    dMasterLookup.Nilai = $('input[name=inp_nilai]').val();
+    dMasterLookup.IsActive = $('input[name=inp_status]:checked').val();
+}
+
+function IndexCreateMasterLookup() {
+    if ($('#created-master-lookup').length) {
+        $('#TambahMasterLookUp').modal('show');
+    } else {
+        $.ajax({
+            url: '/Admin/MasterLookup/ModalCreateMasterLookup',
+            type: 'get',
+            datatype: 'html',
+            success: function (e) {
+                console.log(e);
+                if ($('.data-content-modal').length) {
+                    $('.data-content-modal').remove();
+                }
+                $('#modal-inner').append(e);
+                $('.modal').modal('show');
+                $('.summernote').summernote({
+                    placeholder: 'Input Body Email',
+                    height: 300, // set editor height  
+                    minHeight: null, // set minimum height of editor  
+                });
+            }
+        })
+    }
+}
+
+function IndexUpdateMasterLookup(id) {
+    $.ajax({
+        url: '/Admin/MasterLookup/ModalUpdateMasterLookup/' + id,
+        type: 'get',
+        datatype: 'html',
+        success: function (e) {
+            console.log(e);
+            if ($('.data-content-modal').length) {
+                $('.data-content-modal').remove();
+            }
+            $('#modal-inner').append(e);
+            $('.modal').modal('show');
+            $('.summernote').summernote({
+                placeholder: 'Input Body Email',
+                height: 300, // set editor height  
+                minHeight: null, // set minimum height of editor  
+            });
+        }
+    })
+}
+
+function IndexViewMasterLookup(id) {
+    $.ajax({
+        url: '/Admin/MasterLookup/ModalDetailMasterLookup/' + id,
+        type: 'get',
+        datatype: 'html',
+        success: function (e) {
+            console.log(e);
+            if ($('.data-content-modal').length) {
+                $('.data-content-modal').remove();
+            }
+            $('#modal-inner').append(e);
+            $('.modal').modal('show');
+        }
+    })
+
+}
+
+function PostCreate() {
+    getValueOnForm();
+
+    console.log(dMasterLookup);
+    if (validationCustom()) {
+        var base_url = window.location.origin;
+        $.ajax({
+            url: base_url + '/Admin/MasterLookup/PostDataMasterLookup',
+            type: 'post',
+            datatype: 'json',
+            data: JSON.stringify(dMasterLookup),
+            contentType: 'application/json',
+            success: function (e) {
+                Swal.fire({
+                    title: 'Berhasil',
+                    icon: 'success',
+                    html: 'Template Email Berhasil Ditambahkan',
+                    showCloseButton: true,
+                    showCancelButton: false,
+                    focusConfirm: false,
+                    confirmButtonText: 'OK'
+                })
+                dataTable.ajax.reload(null, false);
+                $('.modal').modal('hide');
+            },
+            error: function (e) {
+                Swal.fire({
+                    title: 'Oppss',
+                    icon: 'error',
+                    html: 'Coba Reload Page',
+                    showCloseButton: true,
+                    showCancelButton: false,
+                    focusConfirm: false,
+                    confirmButtonText: 'OK'
+                })
+                $('.modal').modal('hide');
+            }
+        })
+    } else {
+        Swal.fire({
+            title: 'Oppss',
+            icon: 'warning',
+            html: 'Ada beberapa field yang belum kamu isikan',
+            showCloseButton: true,
+            showCancelButton: false,
+            focusConfirm: false,
+            confirmButtonText: 'OK'
+        })
+    }
+}
+
+function PostUpdate() {
+    getValueOnForm();
+    dMasterLookup.ID = $('#id_MasterLookup').val();
+    console.log(dMasterLookup);
+    var base_url = window.location.origin;
+    $.ajax({
+        url: base_url + '/Admin/MasterLookup/PostUpdateMasterLookup',
+        type: 'post',
+        datatype: 'json',
+        data: JSON.stringify(dMasterLookup),
+        contentType: 'application/json',
+        success: function (e) {
+            Swal.fire({
+                title: 'Berhasil',
+                icon: 'success',
+                html: 'Template Email Berhasil Diubah',
+                showCloseButton: true,
+                showCancelButton: false,
+                focusConfirm: false,
+                confirmButtonText: 'OK'
+            })
+            dataTable.ajax.reload(null, false);
+            $('.modal').modal('hide');
+        },
+        error: function (e) {
+            Swal.fire({
+                title: 'Oppss',
+                icon: 'error',
+                html: 'Coba Reload Page',
+                showCloseButton: true,
+                showCancelButton: false,
+                focusConfirm: false,
+                confirmButtonText: 'OK'
+            })
+            $('.modal').modal('hide');
+        }
+    })
+
+    function DeletedMasterLookup(value) {
+        swal({
+            title: "Are you sure?",
+            text: "You will not be able to recover this imaginary file!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, delete it!",
+            closeOnConfirm: false
+        }, function (isConfirm) {
+            if (!isConfirm) return;
+            $.ajax({
+                url: "scriptDelete.php",
+                type: "POST",
+                data: {
+                    id: 5
+                },
+                dataType: "html",
+                success: function () {
+                    swal("Done!", "It was succesfully deleted!", "success");
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    swal("Error deleting!", "Please try again", "error");
+                }
+            });
+        });
+    }
+
+}
+
