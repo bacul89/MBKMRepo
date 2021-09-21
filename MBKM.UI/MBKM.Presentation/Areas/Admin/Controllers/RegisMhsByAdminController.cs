8e023168939021d719596f42d9bbbd298ad5cab5
RanggaDaga <sendusr@gmail.com>
2021-09-22 04:19:35 +0700

    cicil regis mhs byadmin
    ip wifi ganti ~_~

--- MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/RegisMhsByAdminController.cs
+++ MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/RegisMhsByAdminController.cs
@@ -1,4 +1,5 @@
-﻿using MBKM.Services.MBKMServices;
+﻿using MBKM.Services;
+using MBKM.Services.MBKMServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,12 @@ namespace MBKM.Presentation.Areas.Admin.Controllers
     {
         // GET: Admin/RegisMhsByAdmin
         private IPerjanjianKerjasamaService _perjanjianKerjasamaService;
+        private ILookupService _lookupService;
 
-        public RegisMhsByAdminController(IPerjanjianKerjasamaService perjanjianKerjasamaService)
+        public RegisMhsByAdminController(IPerjanjianKerjasamaService perjanjianKerjasamaService, ILookupService lookupService)
         {
             _perjanjianKerjasamaService = perjanjianKerjasamaService;
+            _lookupService = lookupService;
         }
 
         public ActionResult Index()
@@ -24,6 +27,14 @@ namespace MBKM.Presentation.Areas.Admin.Controllers
         public JsonResult GetNamaInstansi(int Skip, int Length, string Search)
         {
             return Json(_perjanjianKerjasamaService.getNamaInstansi(Skip, Length, Search), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetNoKerjasama(int Skip, int Length, string Search, string NamaInstansi)
+        {
+            return Json(_perjanjianKerjasamaService.getNoKerjasama(Skip, Length, Search, NamaInstansi), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult getLookupByTipe(string tipe)
+        {
+            return Json(_lookupService.getLookupByTipe(tipe), JsonRequestBehavior.AllowGet);
         }
     }
 }
