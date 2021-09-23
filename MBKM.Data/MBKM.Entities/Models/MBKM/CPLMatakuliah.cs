0135cebe1c889efd7776ca9cb506622c6455d10d
bacul <baculsanjose@gmail.com>
2021-09-23 13:48:23 +0700

    commit sprint 2 master"

---
+++ MBKM.Data/MBKM.Entities/Models/MBKM/CPLMatakuliah.cs
@@ -0,0 +1,17 @@
+﻿using MBKM.Entities.Basentities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBKM.Entities.Models.MBKM
+{
+    public class CPLMatakuliah : BaseEntity
+    {
+        public string KodeMataKuliah { get; set; }
+        public string NamaMataKuliah { get; set; }
+        public Int64 CapaianPembelajaranID { get; set; }
+        public MasterCapaianPembelajaran MasterCapaianPembelajarans { get; set; }
+    }
+}
