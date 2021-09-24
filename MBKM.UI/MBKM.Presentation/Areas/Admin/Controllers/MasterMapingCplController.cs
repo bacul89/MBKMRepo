5f83d80aab5ef8db7935eaa5924cc36ad71f0b6c
dkusumandaru <darmakusumandaru@gmail.com>
2021-09-24 16:58:34 +0700

    Master Mapping CPL | init View : Page Datatables, Modal Insert

---
+++ MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/MasterMapingCplController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBKM.Presentation.Areas.Admin.Controllers
+{
+    public class MasterMapingCplController : Controller
+    {
+
+/*        public string arr { get; set; }
+        public string brr { get; set; }
+        public string crr { get; set; }
+        public string drr { get; set; }
+        public string err { get; set; }*/
+
+        // GET: Admin/MasterMapingCpl
+        public ActionResult Index()
+        {
+            Session["username"] = "Smitty Werben Jeger Man Jensen";
+            return View();
+        }
+
+        public class MyJsonObject
+        {
+            [Newtonsoft.Json.JsonProperty("arr")]
+            public string arr { get; set; }
+
+            [Newtonsoft.Json.JsonProperty("brr")]
+            public string brr { get; set; }
+
+            [Newtonsoft.Json.JsonProperty("crr")]
+            public string crr { get; set; }
+
+            [Newtonsoft.Json.JsonProperty("drr")]
+            public string drr { get; set; }
+
+            [Newtonsoft.Json.JsonProperty("err")]
+            public string err { get; set; }
+
+        }
+
+
+        public ActionResult GetDataMasterMapingCpl()
+        {
+            var jsonString = "[{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"},{\"arr\":\"john\",\"brr\":22,\"crr\":\"mca\",\"drr\":\"test\",\"err\":\"hello\"}]";
+            List<MyJsonObject> myJsonObjects = Newtonsoft.Json.JsonConvert.DeserializeObject<List<MyJsonObject>>(jsonString);
+
+            /*var data = _lookupService.Find(x => x.IsDeleted == false);*/
+            return Json(myJsonObjects, JsonRequestBehavior.AllowGet);
+        }
+
+        /*Modal Created*/
+        public ActionResult ModalCreateMasterMapingCpl()
+        {
+            return View("_AddMasterMapingCpl");
+        }
+
+/*        [HttpPost]
+        public ActionResult PostDataMasterMapingCpl()
+        {
+            lookup.CreatedBy = Session["username"] as string;
+            lookup.UpdatedBy = Session["username"] as string;
+            _lookupService.Save(lookup);
+            return Json(lookup);
+        }*/
+
+        /*Modal Update*/
+        public ActionResult ModalUpdateMasterMapingCpl()
+        {
+            
+            return View();
+        }
+
+/*        [HttpPost]
+        public ActionResult PostUpdateMasterMapingCpl(Lookup lookup)
+        {
+
+            Lookup data = _lookupService.Get(lookup.ID);
+            data.Tipe = lookup.Tipe;
+            data.Nama = lookup.Nama;
+            data.Nilai = lookup.Nilai;
+            data.IsActive = lookup.IsActive;
+            data.UpdatedBy = Session["username"] as string;
+
+
+
+            _lookupService.Save(data);
+
+            return Json(data);
+        }*/
+
+/*        [HttpPost]
+        public ActionResult PostDeleteMasterMapingCpl(int id)
+        {
+            var data = _lookupService.Get(id);
+            data.IsDeleted = true;
+            data.UpdatedBy = Session["username"] as string;
+
+            _lookupService.Save(data);
+            return Json(data);
+        }*/
+
+
+
+        /*Modal Detail*/
+        public ActionResult ModalDetailMasterMapingCpl()
+        {
+            
+            return View();
+        }
+
+
+
+    }
+}
