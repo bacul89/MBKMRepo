cc6350d1ef000c22657325ce62f0c8321a952469
RanggaDaga <sendusr@gmail.com>
2021-09-27 12:55:22 +0700

    masterCPL, listMHS view
    cicilan sprint 2 view

---
+++ MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/MasterCPLController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBKM.Presentation.Areas.Admin.Controllers
+{
+    public class MasterCPLController : Controller
+    {
+        // GET: Admin/MasterCPL
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ModaladdCPL()
+        {
+            //IEnumerable<VMLookup> listJabatan = _roleService.getLookupRole();
+            //ViewBag.listJabatan = new SelectList(listJabatan, "Nilai", "Nama");
+
+            //IEnumerable<VMListProdi> listProdi = _lookupService.getListProdi();
+            //ViewBag.listProdi = new SelectList(listProdi, "IDProdi", "NamaProdi");
+
+            //IEnumerable<VMListProdi> listNProdi = _lookupService.getListProdi();
+            //ViewBag.listNProdi = new SelectList(listNProdi, "NamaProdi", "IDProdi");
+
+            return RedirectToAction("Index", "Home", new { area = "Portal" });
+            //Debug.WriteLine("akses index");
+            //return View("ModalCreateCPL");
+        }
+    }
+}
---
+++ MBKM.UI/MBKM.Presentation/Scripts/Admin/MasterCPL/main.js
@@ -0,0 +1,90 @@
+﻿var tableUser = $('#TableList').DataTable({
+    "columnDefs": [{
+        "searchable": false,
+        "orderable": false,
+        "paging": false,
+        "targets": 0,
+        "visible": false, 'targets': [4, 6]
+    }],
+    //"order": [[1, 'asc']],
+    "proccessing": true,
+    "serverSide": true,
+    "order": [[1, 'asc']],
+    //"aaSorting": [[0, "asc"]],
+    "ajax": {
+        url: '/Admin/DaftarSeluruhMahasiswa/GetList',
+        type: 'POST'
+    },
+    "language": {
+        "emptyTable": "No record found.",
+        "processing":
+            '<i class="fa fa-spinner fa-spin fa-3x fa-fw" style="color:#2a2b2b;"></i><span class="sr-only">Loading...</span> ',
+        "search": "Search:",
+        "searchPlaceholder": ""
+    },
+    "columns": [
+        {
+            //"title": "No",
+            "data": null,
+            "render": function (data, type, full, meta) {
+                return meta.row + 1;
+            }
+        },
+        {
+            //"title": "Nomor Induk Pegawai",
+            "data": "Kodes",
+            "name": "Kode",
+            "render": function (data, type, row, meta) {
+                return '<div class="center">' + data + '</div>';
+            }
+        },
+        {
+            //"title": "Nama",
+            "data": "Kelompok",
+            "name": "Kelompok",
+            "render": function (data, type, row, meta) {
+                return '<div class="center">' + data + '</div>';
+            }
+        },
+        {
+            //"title": "Email",
+            "data": "CapaianPembelajaran",
+            "name": "CapaianPembelajaran",
+            "render": function (data, type, row, meta) {
+                return '<div class="center">' + data + '</div>';
+            }
+        },
+       
+
+        {
+            "title": "Action",
+            "data": "ID",
+            "render": function (data, type, row, meta) {
+                return `<div class="row justify-content-center">
+                            <div class="col" style="text-align:center">
+                                
+                               <a href="javascript:void(0)" style="color:black" onclick="EditUserTemplate('${data}')"> <i class="fas fa-edit coral" ></i></a>
+                                <a href="javascript:void(0)" style="color:black" onclick="DetailUserTemplate('${data}')"> <i class="fas fa-file-search coral"></i></a>
+                                <a type="button"  id="btnDel"> <i class="fas fa-trash-alt coral"></i> </button >
+                                
+                                
+                                
+                            </div>
+                        </div>`;//<a href="javascript:void(0)" style="color:black" onclick="DeleteUserGetID('${data}')">  <i class="fas fa-trash-alt coral"></i></a>
+            }
+        },
+
+    ],
+    "createdRow": function (row, data, index) {
+        $('td', row).css({
+            'border': '1px solid coral',
+            'border-collapse': 'collapse',
+            'vertical-align': 'center',
+        });
+
+    }//,
+    //'columnDefs': [
+    //    //hide the second & fourth column
+    //    { 'visible': false, 'targets': [5] }
+    //]
+});
