b8b27b20f85c981a7dfde2b0d3acd9b5e7cc5d06
dkusumandaru <darmakusumandaru@gmail.com>
2021-09-28 10:32:36 +0700

    Master Lookup | disable form detail master lookup

--- MBKM.UI/MBKM.Presentation/Scripts/Admin/MasterMapingCpl/crud.js
+++ MBKM.UI/MBKM.Presentation/Scripts/Admin/MasterMapingCpl/crud.js
@@ -29,33 +29,61 @@ function IndexCreateMasterMapingCpl() {
 }
 
 function IndexUpdateMasterMapingCpl(id) {
+    $.LoadingOverlay("show");
     $.ajax({
         url: '/Admin/MasterMapingCpl/ModalUpdateMasterMapingCpl/' + id,
         type: 'get',
         datatype: 'html',
         success: function (e) {
+            $.LoadingOverlay("hide");
             /*console.log(e);*/
             if ($('.data-content-modal').length) {
                 $('.data-content-modal').remove();
             }
             $('#modal-inner').append(e);
             $('.modal').modal('show');
+        }, error: function (e) {
+            $.LoadingOverlay("hide");
+            Swal.fire({
+                title: 'Oppss',
+                icon: 'error',
+                html: 'Coba Reload Page',
+                showCloseButton: true,
+                showCancelButton: false,
+                focusConfirm: false,
+                confirmButtonText: 'OK'
+            })
+            $('.modal').modal('hide');
         }
     })
 }
 
 function IndexViewMasterMapingCpl(id) {
+    $.LoadingOverlay("show");
     $.ajax({
         url: '/Admin/MasterMapingCpl/ModalDetailMasterMapingCpl/' + id,
         type: 'get',
         datatype: 'html',
         success: function (e) {
+            $.LoadingOverlay("hide");
             /*console.log(e);*/
             if ($('.data-content-modal').length) {
                 $('.data-content-modal').remove();
             }
             $('#modal-inner').append(e);
             $('.modal').modal('show');
+        }, error: function (e) {
+            $.LoadingOverlay("hide");
+            Swal.fire({
+                title: 'Oppss',
+                icon: 'error',
+                html: 'Coba Reload Page',
+                showCloseButton: true,
+                showCancelButton: false,
+                focusConfirm: false,
+                confirmButtonText: 'OK'
+            })
+            $('.modal').modal('hide');
         }
     })
 
