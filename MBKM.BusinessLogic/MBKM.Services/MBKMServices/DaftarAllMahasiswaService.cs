c7d363bfcd53034e0784042a9cf801ed97c23dcd
RanggaDaga <sendusr@gmail.com>
2021-10-05 02:37:16 +0700

    DaftarAllMhs, MasterCPL
    DaftarAllMhs:+ list,detail done,
    - Edit Kerjasama(kurang)
    MasterCPL: -RUD belum berhasil lempar model kedalam modal(action result view return)

---
+++ MBKM.BusinessLogic/MBKM.Services/MBKMServices/DaftarAllMahasiswaService.cs
@@ -0,0 +1,53 @@
+﻿using MBKM.Common.Helpers;
+using MBKM.Common.Interfaces;
+using MBKM.Entities.Models.MBKM;
+using MBKM.Common.Interfaces.RepoInterfaces.MBKMRepoInterfaces;
+using MBKM.Entities.ViewModel;
+using MBKM.Services.BaseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBKM.Services.MBKMServices
+{
+    public interface IDaftarAllMahasiswaService : IEntityService<Mahasiswa>
+    {
+        VMListDaftarAllMahasiswa GetListDaftarAllMahasiswa(DataTableAjaxPostModel model);
+
+
+    }
+    public class DaftarAllMahasiswaService : EntityService<Mahasiswa>, IDaftarAllMahasiswaService
+    {
+        IUnitOfWork _unitOfWork;
+        IDaftarAllMahasiswaRepository _damRepository;
+
+        public DaftarAllMahasiswaService(IUnitOfWork unitOfWork, IDaftarAllMahasiswaRepository DAMRepository)
+            : base(unitOfWork, DAMRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _damRepository = DAMRepository;
+        }
+
+        public VMListDaftarAllMahasiswa GetListDaftarAllMahasiswa(DataTableAjaxPostModel model)
+        {
+            var searchBy = (model.search != null) ? model.search.value : null;
+            var take = model.length;
+            var skip = model.start;
+            string sortBy = "";
+            bool sortDir = true;
+
+            if (model.order != null)
+            {
+                // in this example we just default sort on the 1st column
+                sortBy = model.columns[model.order[0].column].data;
+                sortDir = model.order[0].dir.ToLower() == "asc";
+            }
+            if (sortBy == null)
+                sortBy = "ID";
+            sortBy = sortBy + " " + model.order[0].dir.ToUpper();
+            return _damRepository.GetListAllMhs(skip, take, searchBy, sortBy, sortDir);
+        }
+    }
+}
---
+++ MBKM.Data/MBKM.Repository/Repositories/MBKMRepository/DaftarAllMahasiswaRepository.cs
@@ -0,0 +1,65 @@
+﻿using MBKM.Common.Interfaces.RepoInterfaces.MBKMRepoInterfaces;
+using MBKM.Entities.Models.MBKM;
+using MBKM.Entities.ViewModel;
+using MBKM.Repository.BaseRepository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBKM.Repository.Repositories.MBKMRepository
+{
+    public class DaftarAllMahasiswaRepository : GenericRepository<Mahasiswa>, IDaftarAllMahasiswaRepository
+    {
+        public DaftarAllMahasiswaRepository(DbContext _db) : base(_db)
+        {
+        }
+        public VMListDaftarAllMahasiswa GetListAllMhs(int Skip, int Length, string SearchParam, string SortBy, bool SortDir)
+        {
+            VMListDaftarAllMahasiswa mListMahasiswa = new VMListDaftarAllMahasiswa();
+            if (String.IsNullOrEmpty(SearchParam))
+            {
+                // if we have an empty search then just order the results by Id ascending
+                //SortBy = "ID";
+                //SortDir = true;
+                SearchParam = "";
+            }
+            using (var context = new MBKMContext())
+            {
+
+                var result = context.Mahasiswas.Where(x => x.IsDeleted == false);
+                mListMahasiswa.TotalCount = result.Count();
+                var gridfilter = result
+                    .AsQueryable()
+                    .Where(y => y.NamaUniversitas.Contains(SearchParam) ||
+                                        y.JenjangStudi.Contains(SearchParam) || y.ProdiAsal.Contains(SearchParam) || y.NIMAsal.Contains(SearchParam)
+                                        || y.Nama.Contains(SearchParam) || y.Gender.Contains(SearchParam) || y.NoKerjasama.Contains(SearchParam)
+                                        || y.StatusKerjasama.Contains(SearchParam) || y.StatusVerifikasi.Contains(SearchParam))
+                    .OrderBy(SortBy, SortDir);
+                mListMahasiswa.gridDatas = gridfilter.Skip(Skip).Take(Length)
+                    .Select(z => new GridDataAllMahasiswa
+                    {
+                        ID = z.ID,
+                        Email = z.Email,
+                        Nama = z.Nama,
+                        NamaUniversitas = z.NamaUniversitas,
+                        Gender = context.Lookups.Where(x => x.Tipe == "Gender" && x.Nilai == z.Gender).Select(x => x.Nama).FirstOrDefault(),
+                        NoHp = z.NoHp,
+                        NIMAsal = z.NIMAsal,
+                        NIM = z.NIM,
+                        NoKerjasama = z.NoKerjasama,
+                        StatusKerjasama = z.StatusKerjasama,
+                        Telepon = z.Telepon,
+                        ProdiAsal = z.ProdiAsal,
+                        JenjangStudi = z.JenjangStudi,
+                        StatusVerifikasi = z.StatusVerifikasi
+                    }).ToList();
+                mListMahasiswa.TotalFilterCount = gridfilter.Count();
+                return mListMahasiswa;
+            }
+        }
+    }
+}
