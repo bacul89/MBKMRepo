ea2f3dde5b307ca359125f1d9dca7993ae5da34c
abiyogahendra <abiyogakhusus@gmail.com>
2021-10-13 13:34:49 +0700

    Manage Ujian

---
+++ MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/JadwalUjianController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBKM.Presentation.Areas.Admin.Controllers
+{
+    public class JadwalUjianController : Controller
+    {
+        // GET: Admin/JadwalUjian
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DetailJadwalUjian()
+        {
+            return View();
+        }
+    }
+}
---
+++ MBKM.UI/MBKM.Presentation/Scripts/Admin/ManageUjian/main.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    var table = $('#table-data-manage-ujian-mahasiswa').DataTable();
+
+
+
+})
