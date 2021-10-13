39de99f82055222b0ce328437b1193841694fdb6
bacul <baculsanjose@gmail.com>
2021-10-13 17:22:01 +0700

    tambahmigrasitblabsen

---
+++ MBKM.Data/MBKM.Repository/Migrations/202110131018316_tmbhdosenabsensp3.cs
@@ -0,0 +1,20 @@
+﻿namespace MBKM.Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class tmbhdosenabsensp3 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Absensi", "InstructorId", c => c.String(nullable: false, maxLength: 100, unicode: false));
+            AddColumn("dbo.Absensi", "NamaDosen", c => c.String(nullable: false, maxLength: 250, unicode: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Absensi", "NamaDosen");
+            DropColumn("dbo.Absensi", "InstructorId");
+        }
+    }
+}
