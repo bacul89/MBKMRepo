009af2947d0b889673336933dba8ff90ba3890fc
RanggaDaga <sendusr@gmail.com>
2021-10-18 09:45:42 +0700

    linkfasilitasAdmin
    editLink done, namun seksi blm filter.
    lanjut sisi mahasiswa dulu

--- MBKM.UI/MBKM.Presentation/Scripts/Admin/LinkFasilitasAdmin/crud.js
+++ MBKM.UI/MBKM.Presentation/Scripts/Admin/LinkFasilitasAdmin/crud.js
@@ -23,6 +23,12 @@
                 $("#inp_other").removeAttr("disabled");
                 $("#edit-button").hide();
                 $("#update-button").show();
+                $("#update-button").click(function () {
+                    //var latest_valueKJ = $("option:selected:first", "#noKJ").text();
+                    // $("#noKJ").val(latest_valueKJ);
+                    //alert(latest_valueKJ);
+                    UpdateLink(id);
+                });
             });
 
 
@@ -42,4 +48,43 @@
             $('.modal').modal('hide');
         }
     })
+}
+function UpdateLink(id) {
+    dMasterLink = {}
+   
+   
+    
+    dMasterLink.ID = id;
+    dMasterLink.LinkMoodle = $("#inp_moodle").val();
+    dMasterLink.LinkAtmaZeds = $("#inp_zeds").val();
+    dMasterLink.LinkTeams = $("#inp_teams").val();
+    dMasterLink.LinkOthers = $("#inp_other").val();
+   
+    console.log(dMasterLink);
+    var base_url = window.location.origin;
+    $.ajax({
+        url: base_url + '/Admin/LinkFasilitasAdmin/UpdateLink',
+        type: 'post',
+        datatype: 'json',
+        data: JSON.stringify(dMasterLink),
+        contentType: 'application/json',
+    }).then(function (response) {
+        if (response.status == 200) {
+            Swal.fire({
+                title: 'Berhasil',
+                icon: 'success',
+                html: 'Link Berhasil DiUpdate',
+                showCloseButton: true,
+                showCancelButton: false,
+                focusConfirm: false,
+                confirmButtonText: 'OK'
+            })
+            $('.modal').modal('hide');
+            tableUser.ajax.reload(null, false);
+
+
+        }
+    });
+
+
 }
