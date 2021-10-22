f6c956e73af29a140886de6e3d185cd4dd2ad287
RanggaDaga <sendusr@gmail.com>
2021-10-22 10:23:11 +0700

    BAP Platform (ifnull msh error)
    if viewdata bap.platform null masih error

---
+++ MBKM.BusinessLogic/MBKM.Common/Interfaces/RepoInterfaces/MBKMRepoInterfaces/IReportBAPRepository.cs
@@ -0,0 +1,15 @@
+﻿using MBKM.Entities.Models.MBKM;
+using MBKM.Entities.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBKM.Common.Interfaces.RepoInterfaces.MBKMRepoInterfaces
+{
+    public interface IReportBAPRepository :IGenericRepository<Absensi>
+    {
+        IEnumerable<VMListReportBAP> GetBAPByAbsensiID(int id);
+    }
+}
---
+++ MBKM.Data/MBKM.Entities/ViewModel/VMListReportBAP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBKM.Entities.ViewModel
+{
+    public class VMListReportBAP
+    {
+        public string NIM { get; set; }
+        public string Nama { get; set; }
+        public string NamaUniversitas { get; set; }
+        public string NamaProdi { get; set; }
+        public string NamaMatakuliah { get; set; }
+        public string KodeMataKuliah { get; set; }
+        public string ClassSection { get; set; }
+        public string TANGGAL { get; set; }
+        public string BENTUK { get; set; }
+        public string PLATFORM { get; set; }
+        public string COMMENTS { get; set; }
+        public bool Present { get; set; }
+    }
+}
