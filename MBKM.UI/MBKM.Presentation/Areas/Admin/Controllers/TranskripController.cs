ba3ff59f5865a8a433cc24d89d0c04246295cd03
dkusumandaru <darmakusumandaru@gmail.com>
2021-10-25 18:08:02 +0700

    Transkrip | init FE admin

---
+++ MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/TranskripController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBKM.Presentation.Areas.Admin.Controllers
+{
+    public class TranskripController : Controller
+    {
+        // GET: Admin/Transkrip
+        public ActionResult Index()
+        {
+            Session["username"] = "Smitty Werben Jeger Man Jensen";
+            return View();
+        }
+    }
+}
