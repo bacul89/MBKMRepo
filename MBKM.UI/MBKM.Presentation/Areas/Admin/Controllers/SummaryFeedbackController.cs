804edc4a06f57da9fb51093b0f453d37cb8441c7
abiyogahendra <abiyogakhusus@gmail.com>
2021-10-26 12:22:58 +0700

    Tampilan done

---
+++ MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/SummaryFeedbackController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBKM.Presentation.Areas.Admin.Controllers
+{
+    public class SummaryFeedbackController : Controller
+    {
+        // GET: Admin/SummaryFeedback
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
