498287ca98744aa1dbb9a5009e6fa33ffaf4656b
abiyogahendra <abiyogakhusus@gmail.com>
2021-10-28 00:40:29 +0700

    Post data feedback done

---
+++ MBKM.Data/MBKM.Entities/ViewModel/VMDosenMakulPertemuan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBKM.Entities.ViewModel
+{
+    public class VMDosenMakulPertemuan
+    {
+        public string Instructor_id { get; set; }
+        public string NamaDosen { get; set; }
+    }
+}
