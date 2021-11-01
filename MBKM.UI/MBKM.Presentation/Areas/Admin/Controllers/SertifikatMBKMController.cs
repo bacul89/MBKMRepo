e31d22ca86e0fb72e02e1664c4e342b6f640b05c
abiyogahendra <abiyogakhusus@gmail.com>
2021-11-01 09:59:53 +0700

    sertifikat MBKM

---
+++ MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/SertifikatMBKMController.cs
@@ -0,0 +1,29 @@
+﻿using MBKM.Entities.ViewModel;
+using MBKM.Services.MBKMServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBKM.Presentation.Areas.Admin.Controllers
+{
+    public class SertifikatMBKMController : Controller
+    {
+        private IJadwalUjianMBKMService _jadwalUjianMBKMService;
+
+        public SertifikatMBKMController(IJadwalUjianMBKMService jadwalUjianMBKMService)
+        {
+            _jadwalUjianMBKMService = jadwalUjianMBKMService;
+        }
+
+
+        // GET: Admin/SertifikatMBKM
+        public ActionResult Index()
+        {
+            IEnumerable<VMSemester> data = _jadwalUjianMBKMService.getAllSemester();
+            ViewData["semester"] = data;
+            return View();
+        }
+    }
+}
---
+++ MBKM.UI/MBKM.Presentation/Areas/Portal/Controllers/SertifikatMbkmController.cs
@@ -0,0 +1,32 @@
+﻿using MBKM.Services.MBKMServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MBKM.Presentation.Areas.Portal.Controllers
+{
+    public class SertifikatMbkmController : Controller
+    {
+        private IMahasiswaService _mahasiswaService;
+        private IPendaftaranMataKuliahService _pendaftaranMataKuliahService;
+
+        public SertifikatMbkmController(IMahasiswaService mahasiswaService, IPendaftaranMataKuliahService pendaftaranMataKuliahService)
+        {
+            _mahasiswaService = mahasiswaService;
+            _pendaftaranMataKuliahService = pendaftaranMataKuliahService;
+        }
+
+
+        // GET: Portal/SertifikatMbkm
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PrintData()
+        {
+            return Json(new { dsata = "sds" });
+        }
+    }
+}
