503709310bbeed233744e009fe592594ed752ce1
abiyogahendra <abiyogakhusus@gmail.com>
2021-11-29 10:32:33 +0700

    Tracking and Approval CPL

--- MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/ApprovalPendaftaranMatakuliahController.cs
+++ MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/ApprovalPendaftaranMatakuliahController.cs
@@ -55,7 +55,8 @@ namespace MBKM.Presentation.Areas.Admin.Controllers
             var data = _cPLMKPendaftaranService.Find(x => x.IsDeleted == false && x.PendaftaranMataKuliahID == id).FirstOrDefault();
             var data222 = _cPLMKPendaftaranService.Find(x => x.IsDeleted == false && x.PendaftaranMataKuliahID == id).ToList();
             IList<CPLMatakuliah> tempId = _cPLMatakuliahService.Find(x => x.IDMataKUliah == data.PendaftaranMataKuliahs.JadwalKuliahs.MataKuliahID).ToList();
-            IList<CPLMatakuliah> capaianTujuan = tempId.Where(x => int.Parse(x.MasterCapaianPembelajarans.ProdiID) == data.PendaftaranMataKuliahs.JadwalKuliahs.ProdiID).ToList();
+            /*IList<CPLMatakuliah> capaianTujuan = tempId.Where(x => int.Parse(x.MasterCapaianPembelajarans.NamaProdi) == data.PendaftaranMataKuliahs.JadwalKuliahs.ProdiID).ToList();*/
+            IList<CPLMatakuliah> capaianTujuan = tempId;
             var tmpInformasiPertukaran1 = _informasiPertukaranService.Find(x => x.MahasiswaID == data.PendaftaranMataKuliahs.MahasiswaID).Count();
 
             if (tmpInformasiPertukaran1 == 0)
@@ -95,7 +96,8 @@ namespace MBKM.Presentation.Areas.Admin.Controllers
                 if (data.CPLMatakuliahID != null && !tmpInformasiPertukaran.JenisKerjasama.ToLower().Contains("internal ke luar"))
                 {
                     IList<CPLMatakuliah> tempIDAsal = _cPLMatakuliahService.Find(x => x.IDMataKUliah == data.CPLMatakuliahs.IDMataKUliah).ToList();
-                    IList<CPLMatakuliah> capaianAsal = tempIDAsal.Where(x => x.MasterCapaianPembelajarans.ProdiID == data.CPLMatakuliahs.MasterCapaianPembelajarans.ProdiID).ToList();
+                    /*IList<CPLMatakuliah> capaianAsal = tempIDAsal.Where(x => x.MasterCapaianPembelajarans.ProdiID == data.CPLMatakuliahs.MasterCapaianPembelajarans.ProdiID).ToList();*/
+                    IList<CPLMatakuliah> capaianAsal = tempIDAsal;
                     ViewData["capaianAsal"] = capaianAsal;
                     ViewData["countCPAsal"] = capaianAsal.Count();
 
