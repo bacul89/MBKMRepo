832e7030d5a5117568d0799776eb39c0969326dc
RanggaDaga <sendusr@gmail.com>
2021-12-01 15:37:11 +0700

    Kerjasama Expire hari ini
    tetap bisa di get

--- MBKM.Data/MBKM.Repository/Repositories/MBKMRepository/PerjanjianKerjasamaRepository.cs
+++ MBKM.Data/MBKM.Repository/Repositories/MBKMRepository/PerjanjianKerjasamaRepository.cs
@@ -96,7 +96,7 @@ namespace MBKM.Repository.Repositories.MBKMRepository
             {
                 var today = DateTime.Now;
                 var getJenis = context.JenisKerjasamaModels.Where(x => x.JenisKerjasama.ToLower().Contains("eksternal")).Select(y => y.ID).FirstOrDefault();
-                var result = context.PerjanjianKerjasamas.Where(x => x.NoPerjanjian.Contains(Search) && x.NamaInstansi == NamaInstansi && x.JenisKerjasama ==getJenis.ToString() &&  x.TanggalAkhir >= today)
+                var result = context.PerjanjianKerjasamas.Where(x => x.NoPerjanjian.Contains(Search) && x.NamaInstansi == NamaInstansi && x.JenisKerjasama ==getJenis.ToString() &&  (x.TanggalAkhir >= today || x.TanggalMulai <=today))
                     .OrderBy("NoPerjanjian").Skip(Skip).Take(Length).Select(x => new VMLookupNoKerjasama
                     {
                         ID = x.ID,
