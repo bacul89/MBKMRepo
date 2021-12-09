5c5f88a85708a7b7aaa062aa85b79a719f787f98
abiyogahendra <abiyogakhusus@gmail.com>
2021-12-09 10:52:49 +0700

    Dosen tidak bisa menambah penilaian untuk matakuliah yang bukan miliknya

--- MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/PenilaianController.cs
+++ MBKM.UI/MBKM.Presentation/Areas/Admin/Controllers/PenilaianController.cs
@@ -53,14 +53,31 @@ namespace MBKM.Presentation.Areas.Admin.Controllers
         public ActionResult GetKelas(string jenjangStudi, string fakultas, string lokasi, string prodi, string matkul, string seksi, int strm)
         {
             var noPegawai = int.Parse(HttpContext.Session["nopegawai"].ToString());
+            var RoleUser = HttpContext.Session["nopegawai"].ToString().ToLower();
             var result = new List<JadwalKuliah>();
-            if (seksi == null || seksi.Length == 0)
+            if(RoleUser.Contains("dosen") || RoleUser.Contains("program studi"))
             {
-                result = _jadwalKuliahService.Find(_ => _.JenjangStudi == jenjangStudi && _.NamaFakultas == fakultas && _.Lokasi == lokasi && _.NamaProdi == prodi && _.KodeMataKuliah + " - " + _.NamaMataKuliah == matkul && _.FlagOpen && _.STRM == strm && _.DosenID == noPegawai).ToList();
-            } else
-            {
-                result = _jadwalKuliahService.Find(_ => _.JenjangStudi == jenjangStudi && _.NamaFakultas == fakultas && _.Lokasi == lokasi && _.NamaProdi == prodi && _.KodeMataKuliah + " - " + _.NamaMataKuliah == matkul && _.ClassSection == seksi && _.FlagOpen && _.STRM == strm && _.DosenID == noPegawai).ToList();
+                if (seksi == null || seksi.Length == 0)
+                {
+                    result = _jadwalKuliahService.Find(_ => _.JenjangStudi == jenjangStudi && _.NamaFakultas == fakultas && _.Lokasi == lokasi && _.NamaProdi == prodi && _.KodeMataKuliah + " - " + _.NamaMataKuliah == matkul && _.FlagOpen && _.STRM == strm && _.DosenID == noPegawai).ToList();
+                }
+                else
+                {
+                    result = _jadwalKuliahService.Find(_ => _.JenjangStudi == jenjangStudi && _.NamaFakultas == fakultas && _.Lokasi == lokasi && _.NamaProdi == prodi && _.KodeMataKuliah + " - " + _.NamaMataKuliah == matkul && _.ClassSection == seksi && _.FlagOpen && _.STRM == strm && _.DosenID == noPegawai).ToList();
+                }
             }
+            else
+            {
+                if (seksi == null || seksi.Length == 0)
+                {
+                    result = _jadwalKuliahService.Find(_ => _.JenjangStudi == jenjangStudi && _.NamaFakultas == fakultas && _.Lokasi == lokasi && _.NamaProdi == prodi && _.KodeMataKuliah + " - " + _.NamaMataKuliah == matkul && _.FlagOpen && _.STRM == strm).ToList();
+                }
+                else
+                {
+                    result = _jadwalKuliahService.Find(_ => _.JenjangStudi == jenjangStudi && _.NamaFakultas == fakultas && _.Lokasi == lokasi && _.NamaProdi == prodi && _.KodeMataKuliah + " - " + _.NamaMataKuliah == matkul && _.ClassSection == seksi && _.FlagOpen && _.STRM == strm).ToList();
+                }
+            }
+           
             return new ContentResult { Content = JsonConvert.SerializeObject(result), ContentType = "application/json" };
         }
         public ActionResult GetBobot(string idMatkul)
