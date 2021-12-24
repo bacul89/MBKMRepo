3136d20e76eb9d5d05cc30d775fae2606d218ca1
RanggaDaga <sendusr@gmail.com>
2021-12-24 14:26:46 +0700

    DHK add list ruangan

---
+++ MBKM.Data/MBKM.Entities/ViewModel/VMListRuangan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBKM.Entities.ViewModel
+{
+    public class VMListRuangan
+    {
+        public string Nama { get; set; }
+    }
+}
